42f86c9b9c1fdeaeff82e7242a3d2bc21537dba1
mehakbasrani <55041907+mehakbasrani@users.noreply.github.com>
2021-03-26 19:42:19 +0530

    web development assignment

---
+++ day 4 assignment/fetching.js
@@ -0,0 +1,11 @@
+var tagName=document.getElementsByTagName('h1');
+tagName[0].innerHTML="<h1>Hello!! Its Mehak Basrani</h1>";
+
+
+
+var Class=document.getElementsByClassName('intro');
+Class[0].innerHTML="Having a nice journey of learning with LetsUpgrade";
+
+
+var Id=document.getElementById('contact');
+Id.innerHTML="You can contact me on my mailid-<a href=>mehakbasrani17@gmail.com<a>";
